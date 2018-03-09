f25fc21af927c70286731e4d1363a84b5eec6a97
Eric Chang <chiahan1123@gmail.com>
2018-03-09 13:30:49 +0800

    Add simple web server and Dockerfile

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"math"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	x := 0.0001
+	for i := 0; i <= 1000000; i++ {
+		x += math.Sqrt(x)
+	}
+	w.Write([]byte("OK!"))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
